f7df4cb57a247422f43d15492a4db902b68fc33b
3jamees <jamesiiking8@gmail.com>
2026-02-19 17:01:46 -0700

    Created main program

--- ConsoleAppEmpty/Program.cs
+++ ConsoleAppEmpty/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello World!");
+﻿using GameEnums;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Select a difficulty (Easy, Medium, Hard):");
+        GameDifficulty difficulty;
+        Enum.TryParse(Console.ReadLine(), true, out difficulty);
+
+        Console.WriteLine();
+        Console.WriteLine("Select a character class (Warrior, Mage, Rogue):");
+        CharacterClass characterClass;
+        Enum.TryParse(Console.ReadLine(), true, out characterClass);
+
+        Console.WriteLine();
+        Console.WriteLine("You selected:");
+        Console.WriteLine("Difficulty: " + difficulty);
+        Console.WriteLine("Character Class: " + characterClass);
+    }
+}
